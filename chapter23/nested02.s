c4861833d10c1c5cc0dabea7a379a922693b3f8c
Roger Ferrer Ibanez <rofirrim@gmail.com>
2015-01-02 11:29:35 +0100

    More progress

--- chapter23/nested02.s
+++ chapter23/nested02.s
@@ -35,15 +35,15 @@ f:
     sub sp, sp, #8      /* make room for x (4 + 4 bytes) */
     /* x will be in address "fp - 4" */
 
-   /* At this point our stack looks like this
+    /* At this point our stack looks like this
 
      Data | Address | Notes
     ------+---------+---------------------------
           | fp - 8  | alignment (per AAPCS)
       x   | fp - 4  | 
       r4  | fp      |  
-      r10 | fp + 8  | old value of r10
-      fp  | fp + 12 | old value of fp
+      r10 | fp + 8  | previous value of r10
+      fp  | fp + 12 | previous value of fp
       lr  | fp + 16 |
    */
 
@@ -60,107 +60,107 @@ f:
     pop {r4, r10, fp, lr}  /* restore registers */
     bx lr /* return */
 
-    /* ------ nested function ------------------ */
-    h :
-        push {r4, r5, r10, fp, lr} /* keep registers */
-        mov fp, sp /* setup dynamic link */
+/* ------ nested function ------------------ */
+h :
+    push {r4, r5, r10, fp, lr} /* keep registers */
+    mov fp, sp /* setup dynamic link */
 
-        sub sp, sp, #4 /* align stack */
+    sub sp, sp, #4 /* align stack */
 
-        /* At this point our stack looks like this
+    /* At this point our stack looks like this
 
-          Data | Address | Notes
-         ------+---------+---------------------------
-               | fp - 4  | alignment (per AAPCS)
-           r4  | fp      |  
-           r5  | fp + 4  | 
-           r10 | fp + 8  | frame pointer of 'f'
-           fp  | fp + 12 | frame pointer of caller
-           lr  | fp + 16 |
-        */
+      Data | Address | Notes
+     ------+---------+---------------------------
+           | fp - 4  | alignment (per AAPCS)
+       r4  | fp      |  
+       r5  | fp + 4  | 
+       r10 | fp + 8  | frame pointer of 'f'
+       fp  | fp + 12 | frame pointer of caller
+       lr  | fp + 16 |
+    */
 
-        /* prepare call to g */
-        /* g is a sibling so the static link will be the same
-           as the current one */
-        ldr r10, [fp, #8]
-        bl g
+    /* prepare call to g */
+    /* g is a sibling so the static link will be the same
+       as the current one */
+    ldr r10, [fp, #8]
+    bl g
 
-        /* prepare call to m */
-        /* m is an immediately nested function so the static
-           link is the current frame */
-        mov r10, fp
-        bl m
+    /* prepare call to m */
+    /* m is an immediately nested function so the static
+       link is the current frame */
+    mov r10, fp
+    bl m
 
-        ldr r4, [fp, #8]  /* load frame pointer of 'f' */
-        ldr r5, [r4, #-4]  /* r5 ← x */
-        add r5, r5, #3     /* r5 ← r5 + 3 */
-        str r5, [r4, #-4]  /* x ← r5 */
+    ldr r4, [fp, #8]  /* load frame pointer of 'f' */
+    ldr r5, [r4, #-4]  /* r5 ← x */
+    add r5, r5, #3     /* r5 ← r5 + 3 */
+    str r5, [r4, #-4]  /* x ← r5 */
 
-        mov sp, fp            /* restore stack */
-        pop {r4, r5, r10, fp, lr} /* restore registers */
-        bx lr
+    mov sp, fp            /* restore stack */
+    pop {r4, r5, r10, fp, lr} /* restore registers */
+    bx lr
 
 
-        /* ------ nested function ------------------ */
-        m:
-            push {r4, r5, r10, fp, lr} /* keep registers */
-            mov fp, sp /* setup dynamic link */
+/* ------ nested function ------------------ */
+m:
+    push {r4, r5, r10, fp, lr} /* keep registers */
+    mov fp, sp /* setup dynamic link */
 
-            sub sp, sp, #4 /* align stack */
-            /* At this point our stack looks like this
+    sub sp, sp, #4 /* align stack */
+    /* At this point our stack looks like this
 
-              Data | Address | Notes
-             ------+---------+---------------------------
-                   | fp - 4  | alignment (per AAPCS)
-               r4  | fp      |  
-               r5  | fp + 4  |
-               r10 | fp + 8  | frame pointer of 'h'
-               fp  | fp + 12 | frame pointer of caller
-               lr  | fp + 16 |
-            */
+      Data | Address | Notes
+     ------+---------+---------------------------
+           | fp - 4  | alignment (per AAPCS)
+       r4  | fp      |  
+       r5  | fp + 4  |
+       r10 | fp + 8  | frame pointer of 'h'
+       fp  | fp + 12 | frame pointer of caller
+       lr  | fp + 16 |
+    */
 
-            ldr r4, [fp, #8]  /* r4 ← frame pointer of 'h' */
-            ldr r4, [r4, #8]  /* r4 ← frame pointer of 'f' */
-            ldr r5, [r4, #-4] /* r5 ← x */
-            add r5, r5, #2    /* r5 ← r5 + 2 */
-            str r5, [r4, #-4] /* x ← r5 */
+    ldr r4, [fp, #8]  /* r4 ← frame pointer of 'h' */
+    ldr r4, [r4, #8]  /* r4 ← frame pointer of 'f' */
+    ldr r5, [r4, #-4] /* r5 ← x */
+    add r5, r5, #2    /* r5 ← r5 + 2 */
+    str r5, [r4, #-4] /* x ← r5 */
 
-            /* setup call to g */
-            ldr r10, [fp, #8]   /* r10 ← frame pointer of 'h' */
-            ldr r10, [r10, #8]  /* r10 ← frame pointer of 'f' */
-            bl g
+    /* setup call to g */
+    ldr r10, [fp, #8]   /* r10 ← frame pointer of 'h' */
+    ldr r10, [r10, #8]  /* r10 ← frame pointer of 'f' */
+    bl g
 
-            mov sp, fp                /* restore stack */
-            pop {r4, r5, r10, fp, lr} /* restore registers */
-            bx lr
+    mov sp, fp                /* restore stack */
+    pop {r4, r5, r10, fp, lr} /* restore registers */
+    bx lr
 
-    /* ------ nested function ------------------ */
-    g:
-        push {r4, r5, r10, fp, lr} /* keep registers */
-        mov fp, sp /* setup dynamic link */
+/* ------ nested function ------------------ */
+g:
+    push {r4, r5, r10, fp, lr} /* keep registers */
+    mov fp, sp /* setup dynamic link */
 
-        sub sp, sp, #4 /* align stack */
+    sub sp, sp, #4 /* align stack */
 
-        /* At this point our stack looks like this
+    /* At this point our stack looks like this
 
-          Data | Address | Notes
-         ------+---------+---------------------------
-               | fp - 4  | alignment (per AAPCS)
-           r4  | fp      |  
-           r5  | fp + 4  |  
-           r10 | fp + 8  | frame pointer of 'f'
-           fp  | fp + 12 | frame pointer of caller
-           lr  | fp + 16 |
-        */
+      Data | Address | Notes
+     ------+---------+---------------------------
+           | fp - 4  | alignment (per AAPCS)
+       r4  | fp      |  
+       r5  | fp + 4  |  
+       r10 | fp + 8  | frame pointer of 'f'
+       fp  | fp + 12 | frame pointer of caller
+       lr  | fp + 16 |
+    */
 
-        ldr r4, [fp, #8]  /* r4 ← frame pointer of 'f' */
-        ldr r5, [r4, #-4] /* r5 ← x */
-        add r5, r5, #1    /* r5 ← r5 + 1 */
-        str r5, [r4, #-4] /* x ← r5 */
+    ldr r4, [fp, #8]  /* r4 ← frame pointer of 'f' */
+    ldr r5, [r4, #-4] /* r5 ← x */
+    add r5, r5, #1    /* r5 ← r5 + 1 */
+    str r5, [r4, #-4] /* x ← r5 */
 
-        mov sp, fp /* restore dynamic link */
-        pop {r4, r5, r10, fp, lr} /* restore registers */
-        bx lr
+    mov sp, fp /* restore dynamic link */
+    pop {r4, r5, r10, fp, lr} /* restore registers */
+    bx lr
 
 .globl main
 
